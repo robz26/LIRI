50f761bd7060a4ac66b1d3945120d6692e96e93e
Robin Zhao <37418663+robz26@users.noreply.github.com>
2018-07-14 00:39:24 -0500

    completed, do-what-it-says commands still needs more testing

--- keys.js
+++ keys.js
@@ -1,15 +1,15 @@
-console.log('this is loaded');
+
+
+// console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.quu2mlzO4a9RInzrJRR4hfmCw,
-  consumer_secret: process.env.nyZGP5fVp22N4J5eGdyXNKSLlfSym0uxIOWX9oEZw3rlmnGp25
-  ,
-  access_token_key: process.env.1017873087772184577-iSpkaKkwPczKSumQBD40XlmJDbvEHL,
-  access_token_secret: process.env.3ip2OOeb9GorwgJPx8yRBvLLYWkQ9EZNCmQ0dIgZXWHwY
-
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.d5325725bfbd45fbaba8bc60ec99e366,
-  secret: process.env.0c0ba4a153254891a040037eae8578dc
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -2,32 +2,129 @@ require("dotenv").config();
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require("request");
-var keys = require('./keys');
+var keys = require('./keys.js');
+var fs = require('fs');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-var params = { screen_name: 'nodejs' };
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-        console.log(tweets);
+
+
+
+
+
+
+if (process.argv[2] === 'my-tweets') {
+    var params = { veryimportant1: 'nodejs' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (let i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+                console.log(tweets[i].created_at);
+            }
+        } else {
+            console.log(error);
+        }
+    });
+
+}
+
+if (process.argv[2] === 'spotify-this-song') {
+    var song = JSON.stringify(process.argv[3]);
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            if (response.tracks.total !== 0) {
+                console.log(response.tracks.items[0].album.artists[0].name);
+                console.log(response.tracks.items[0].name);
+                console.log(response.tracks.items[0].external_urls.spotify);
+                console.log(response.tracks.items[0].album.name);
+            } else {
+                spotify.search({ type: 'track', query: 'Ace of Base The Sign' })
+                    .then(function (response) {
+
+                        console.log(response.tracks.items[0].album.artists[0].name);
+                        console.log(response.tracks.items[0].name);
+                        console.log(response.tracks.items[0].external_urls.spotify);
+                        console.log(response.tracks.items[0].album.name);
+                    })
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+
+}
+if (process.argv[2] === 'movie-this') {
+    var movieName = '';
+    for (var i = 3; i < process.argv.length; i++) {
+
+        movieName += process.argv[i] + " ";
     }
-});
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
+    //   console.log(movieName);
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            // console.log(body);
 
-    console.log(data);
-});
+            if (body !== '{"Response":"False","Error":"Movie not found!"}') {
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("The release year is: " + JSON.parse(body).Year);
+                console.log("Rated: " + JSON.parse(body).Rated);
+                console.log("RT Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Country of Origin: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+            } else {
+                queryUrl = "http://www.omdbapi.com/?i=tt0485947&y=&plot=short&apikey=trilogy";
+                request(queryUrl, function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        console.log("Title: " + JSON.parse(body).Title);
+                        console.log("The release year is: " + JSON.parse(body).Year);
+                        console.log("Rated: " + JSON.parse(body).Rated);
+                        console.log("RT Rating: " + JSON.parse(body).Ratings[1].Value);
+                        console.log("Country of Origin: " + JSON.parse(body).Country);
+                        console.log("Language: " + JSON.parse(body).Language);
+                        console.log("Plot: " + JSON.parse(body).Plot);
+                        console.log("Actors: " + JSON.parse(body).Actors);
+                    }
+                })
+            }
+        }
+    })
+}
+if (process.argv[2] === 'do-what-it-says') {
 
-var movieName = process.argv[3];
+    fs.readFile('random.txt', 'UTF-8', function (err, data) {
+        if (!err) {
+            // console.log(data);
+            dataArray = data.split(',');
+            // console.log(dataArray);
+            // process.argv.push(dataArray[0])
+            // process.argv.push(dataArray[1])
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-request(queryUrl, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log(JSON.stringify(body, null, 2));
-        console.log("The release year is: " + JSON.parse(body).Year);
-    }
-})
+            switch (dataArray[0]) {
+                case 'spotify-this-song':
+                    spotify
+                        .search({ type: 'track', query: dataArray[1] })
+                        .then(function (response) {
+                            if (response.tracks.total !== 0) {
+                                console.log(response.tracks.items[0].album.artists[0].name);
+                                console.log(response.tracks.items[0].name);
+                                console.log(response.tracks.items[0].external_urls.spotify);
+                                console.log(response.tracks.items[0].album.name);
+                            }
+
+                        })
+                    break;
+                default:
+                    console.log("nothing found");
+            }
+            // console.log(process.argv);
+        }
+    })
+
+}
